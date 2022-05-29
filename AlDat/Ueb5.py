3bcfa66b71eeba8164a3d60e4f4043ea9fdf7888
Minutenreis <minutenreis@gmail.com>
2022-05-29 21:57:40 +0200

    Ueb5Algodat

--- AlDat/Ueb5.py
+++ AlDat/Ueb5.py
@@ -1,36 +1,31 @@
-def split(C: list, a: int, b:int) -> list:
-    C1 = []
-    for i in range(a, b):
-        C1.append(C[i])
-    return C1
-
-def getMedian(C: list, n: int) -> float:
+def getMedian(C: list, CIndex: int, n: int) -> float:
     if (n % 2 == 0):
-        return (C[n//2 - 1] + C[n//2])/2
+        return (C[CIndex + n//2 - 1] + C[CIndex + n//2])/2
     else:
-        return C[n//2]
+        return C[CIndex + n//2]
 
-def median(A: list, B: list, n: int) -> float:
-    k = n % 2;
+
+def median(A: list, AIndex: int, B: list, BIndex: int, n: int) -> float:
     if(n == 0):
         return 0
     if(n == 1):
-        return (A[0] + B[0])/2
-    if(n==2):
-        C = A+B
+        return (A[AIndex] + B[BIndex])/2
+    if(n == 2):
+        C = [A[AIndex], A[AIndex+1], B[BIndex], B[BIndex+1]]
         C.sort()
-        return getMedian(C,2*n)
-       
-    m1 = getMedian(A, n)
-    m2 = getMedian(B, n)
-   
+        return getMedian(C,0, 4)
+
+    m1 = getMedian(A, AIndex, n)
+    m2 = getMedian(B, BIndex, n)
+
     if(m1 == m2):
         return m1
     if (m1 < m2):
-        return median(split(A,n//2,n), split(B,0,n//2+k), n - n//2)
+        return median(A, AIndex+n//2, B, BIndex, n - n//2)
     else:
-        return median(split(A,0,n//2+k), split(B,n//2,n), n - n//2)
+        return median(A, AIndex, B, BIndex + n//2, n - n//2)
 
-A = [-1,1,2,5,9]
-B = [7,8,10,11,12]
-print(median(A, B, len(A)))
+
+A = [1,3,6,12,14]
+B = [2,5,7,8,10]
+print(median(A, 0, B, 0, len(A)))
